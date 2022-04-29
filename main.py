606d09e38150a52f6913f65fc01d0dfacccd77c8
joaodath <joaorodriguesdiasneto@gmail.com>
2022-04-29 16:42:47 -0300

    chore: create basic api structure

--- main.py
+++ main.py
@@ -3,7 +3,21 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 def index():
     return {"message": "Hello World"}
 
+# add basic structure to api
+
+
+@app.post("/graph") #save graph
+def post_graph(graph: dict):
+    return {"graph": graph}
+
+
+@app.get("/graph/{graph_id}") #recover graph
+def get_graph(graph_id: int):
+    return {"graph_id": graph_id}
+
+# todo: request clarification about the last two endpoints
