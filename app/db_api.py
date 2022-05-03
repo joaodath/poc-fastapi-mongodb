8d2afbe1de78be1a1bf36aafd8e3c8a389f6fe6c
joaodath <joaorodriguesdiasneto@gmail.com>
2022-05-03 16:14:52 -0300

    feat: change from pymongo to motor

--- app/db_api.py
+++ app/db_api.py
@@ -1,14 +1,17 @@
+import os
+from dotenv import load_dotenv, find_dotenv
+
+#needed to connect with mongodb
+import motor.motor_asyncio
+
+load_dotenv(find_dotenv())
+
+
 def get_database():
-  import os
-  from dotenv import load_dotenv, find_dotenv
-  from pymongo import mongo_client
-  import pymongo
+    #connection_string = f'mongodb://mongodb1:27017,mongodb2:27017,mongodb3:27017/?replicaSet=rsmongo'
+    client = motor.motor_asyncio.AsyncIOMotorClient(os.environ["MONGODBURI"])
 
-  load_dotenv(find_dotenv())
-  connection_string = f'mongodb://mongodb1:27017,mongodb2:27017,mongodb3:27017/{os.getenv("MONGODB")}?replicaSet=rsmongo'
-  client = mongo_client.MongoClient(connection_string)
-
-  return client
+    return client
 
 if __name__ == "__main__":
-  mongodb = get_database()
+    get_database()
