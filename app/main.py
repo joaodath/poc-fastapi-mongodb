c56813e783274ccc53020d23f981d95d0cb2d98b
joaodath <joaorodriguesdiasneto@gmail.com>
2022-05-03 21:38:03 -0300

    feat: use odmantic instead of motor directly

--- app/main.py
+++ app/main.py
@@ -1,23 +1,25 @@
-from typing import Optional
+import os
+from typing import List, Optional
 from fastapi import FastAPI
+
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv(raise_error_if_not_found=True))
+
+from .database.database import find_all
+from .database.models import Graph, GraphList
 
 app = FastAPI()
 
-
-@app.get("/")
-def index():
-    return {"message": "Hello World"}
-
+@app.get("/", description="A route to get all graphs stored", response_model=List[GraphList])
+async def find_all():
+    graphs = await find_all(GraphList)
+    return graphs
 # add basic structure to api
 
-
-@app.post("/graph") #save graph
-def post_graph(graph: dict):
-    return {"graph": graph}
-
-
-@app.get("/graph/{graph_id}") #recover graph
+@app.get("/graph/{graph_id}")  # recover graph
 def get_graph(graph_id: int):
+
     return {"graph_id": graph_id}
 
 # todo: request clarification about the last two endpoints
